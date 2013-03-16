c19a729cf8ce7a0999e41aed954d2ae8dc4216ce
zero <wangsong1233276@126.com>
2013-03-16 19:11:24 +0800

    jquery ui

--- openerp/addons/web/static/lib/jquery.ui/js/jquery-ui-1.9.1.custom.js
+++ openerp/addons/web/static/lib/jquery.ui/js/jquery-ui-1.9.1.custom.js
@@ -6455,17 +6455,17 @@ function Datepicker() {
 	this._dayOverClass = 'ui-datepicker-days-cell-over'; // The name of the day hover marker class
 	this.regional = []; // Available regional settings, indexed by language code
 	this.regional[''] = { // Default regional settings
-		closeText: 'Done', // Display text for close link
-		prevText: 'Prev', // Display text for previous month link
-		nextText: 'Next', // Display text for next month link
-		currentText: 'Today', // Display text for current month link
+		closeText: '完成', // Display text for close link
+		prevText: '向前', // Display text for previous month link
+		nextText: '向后', // Display text for next month link
+		currentText: '今天', // Display text for current month link
 		monthNames: ['January','February','March','April','May','June',
 			'July','August','September','October','November','December'], // Names of months for drop-down and formatting
-		monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], // For formatting
+		monthNamesShort: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'], // For formatting
 		dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'], // For formatting
 		dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'], // For formatting
 		dayNamesMin: ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'], // Column headings for days starting at Sunday
-		weekHeader: '周', // Column header for week of the year
+		weekHeader: '周数', // Column header for week of the year
 		dateFormat: 'mm/dd/yy', // See format options on parseDate
 		firstDay: 0, // The first day of the week, Sun = 0, Mon = 1, ...
 		isRTL: false, // True if right-to-left language, false if left-to-right
